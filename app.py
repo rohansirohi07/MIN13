fd8d5e1a2a24b8bf97bb2206ffaad0da1255203c
rohansirohi7 <sirohirohan771@gmail.com>
2026-05-22 16:11:32 +0530

    add the updation

--- app.py
+++ app.py
@@ -205,5 +205,7 @@ def generate_fallback_data():
         })
     return data
 
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False)
